229119d9c80e589cafc03a25f8105d0c103207c9
Ryan Saul <seccom@gmail.com>
2015-08-24 23:38:48 -0700

    added post details page

--- easy_blog_django/posts/models.py
+++ easy_blog_django/posts/models.py
@@ -6,3 +6,5 @@ from ..core.models import TimeStampedModel
 class Post(TimeStampedModel):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
+    # TODO: Add author field
+    #createdBy = models.ForeignKey(User, blank=True, null=True)
