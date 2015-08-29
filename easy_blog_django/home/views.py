e55c423a3133c97db1e0a2f76664c05842c45393
Ryan Saul <seccom@gmail.com>
2015-08-29 11:08:59 -0700

    added published attribute along with manager

--- easy_blog_django/home/views.py
+++ easy_blog_django/home/views.py
@@ -4,11 +4,14 @@ from ..posts.models import Post
 
 
 class LastPostMixIn(object):
-
+    """
+    Returns the most recent post. Only returns a single post.
+    """
     def get_context_data(self, **kwargs):
         context = super(LastPostMixIn, self).get_context_data(**kwargs)
         try:
-            last_post = Post.objects.latest('created')
+            last_post = Post.objects.published()
+            last_post = last_post.latest('created')
             context['last_post'] = last_post
             return context
         except Post.DoesNotExist, does_not_exist:
@@ -17,6 +20,22 @@ class LastPostMixIn(object):
             raise multiple_objects_returned
 
 
-class HomeView(LastPostMixIn, TemplateView):
+class MostRecentPostsMixIn(object):
+    """
+    Returns the most recent posts up to the specified amount.
+    easy_blog_django default is set to 10.
+    """
+    def get_context_data(self, **kwargs):
+        context = super(MostRecentPostsMixIn, self).get_context_data(**kwargs)
+        try:
+            most_recent_posts = Post.objects.published()
+            most_recent_posts = most_recent_posts.order_by('-created')[:10]
+            context['most_recent_posts'] = most_recent_posts
+            return context
+        except Post.DoesNotExist, does_not_exist:
+            raise does_not_exist
+
+
+class HomeView(LastPostMixIn, MostRecentPostsMixIn, TemplateView):
     template_name = "pages/home.html"
 
--- easy_blog_django/posts/models.py
+++ easy_blog_django/posts/models.py
@@ -3,8 +3,20 @@ from django.db import models
 from ..core.models import TimeStampedModel
 
 
+class PostManager(models.Manager):
+
+    use_for_related_fields = True
+
+    def published(self, **kwargs):
+        return self.filter(published__exact=True, **kwargs)
+
+
 class Post(TimeStampedModel):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
+    published = models.BooleanField(default=False)
     # TODO: Add author field
     #author = models.ForeignKey(User, blank=True, null=True)
+
+    # Add custom model manager
+    objects = PostManager()
