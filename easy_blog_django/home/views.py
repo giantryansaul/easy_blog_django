72dcf8af340e8b2d1a75462a19f9d581908c621b
Ryan Saul <seccom@gmail.com>
2015-09-16 23:58:03 -0700

    removed jumbotron from home view, importing styles from clean blog template

--- easy_blog_django/home/views.py
+++ easy_blog_django/home/views.py
@@ -3,25 +3,7 @@ from django.views.generic import ListView
 from ..posts.models import Post
 
 
-class LastPostMixIn(object):
-    """
-    Returns the most recent post. Only returns a single post.
-    """
-    # TODO: Use for jumbotron
-    def get_context_data(self, **kwargs):
-        context = super(LastPostMixIn, self).get_context_data(**kwargs)
-        try:
-            last_post = Post.objects.published()
-            last_post = last_post.latest('created')
-            context['last_post'] = last_post
-            return context
-        except Post.DoesNotExist, does_not_exist:
-            raise does_not_exist
-        except Post.MultipleObjectsReturned, multiple_objects_returned:
-            raise multiple_objects_returned
-
-
-class HomeView(LastPostMixIn, ListView):
+class HomeView(ListView):
 
     template_name = "pages/home.html"
 
